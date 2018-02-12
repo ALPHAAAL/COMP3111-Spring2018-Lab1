b54161888f9c06543ab209829ac40a5f90535988
Alex <alexlau@benovelty.com>
2018-02-12 14:12:01 +0800

    Small tweaks

--- src/labs/comp3111/ust/hk/Lab1Activity.java
+++ src/labs/comp3111/ust/hk/Lab1Activity.java
@@ -1,10 +1,37 @@
 package labs.comp3111.ust.hk;
 
 public class Lab1Activity {
-	public static void main(String[] args) {
-		int[] arr = {1,2,3,4,5,6,7,8,9,10};
+	public static int max(int[] arr) {
+		int max = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		return max;
+	}
+
+	public static int min(int[] arr) {
+		int min = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		return min;
+	}
+	
+	public static int sum(int[] arr) {
 		int sum=0;
-		System.out.println(String.format("Sum of the number is %d", sum));
-		System.out.println("Min = 1; Max = 10");
+		for (int i=0; i<arr.length; i++) {
+			sum+=arr[i];
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		System.out.println("Sum of the number is " + sum(arr));
+		System.out.println("Min = " + min(arr) + "; Max = " + max(arr));
 	}
 }
